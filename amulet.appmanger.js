ce6612075b3217905d3fa87c5f7018222ede20af
prehensile <prehensile@lima>
2024-04-28 10:07:43 +0100

    started appmanager

---
+++ amulet.appmanger.js
@@ -0,0 +1,12 @@
+Bangle.appManager = {
+    STATES : {
+        LOADING: 0,
+        TRANSITION : 1,
+        APP : 2
+    },
+    APP_CURRENT : null,
+    APP_INCOMING : null,
+    STATE_CURRENT : null,
+    buffers : [],
+    loadApp(src) : 
+}
